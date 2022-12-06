ac158efc5313c9286d2d6dcc45f8ac5a9eee36c4
MaxKov86 <max.kov86@ukr.net>
2022-12-06 13:38:08 +0300

    Update  1 task JS and 2 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+import '../css/common.css';
 
+const refs = {
+    startBtn: document.querySelector(`button[data-start]`),
+    stopBtn: document.querySelector(`button[data-stop]`)
+}
+
+
+refs.startBtn.addEventListener(`click`, onStartBtnClick)
+refs.stopBtn.addEventListener(`click`, onStopBtnClick)
+
+
+let intervalId = null;
+
+function onStartBtnClick() {
+intervalId = setInterval(() => {
+document.body.style.backgroundColor = getRandomHexColor()
+}, 1000)
+    
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+};
+
+function onStopBtnClick() {
+    clearInterval(intervalId);
+
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
